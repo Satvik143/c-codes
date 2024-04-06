c564d819472e6a53fb07c222779f17ef867c5a70
Satvik M Patil <satvikpatil2012@gmail.com>
2024-04-06 14:49:05 +0530

    added coments to the code

--- palindrome.c
+++ palindrome.c
@@ -1,16 +1,27 @@
+//imports standard input - output functions
 #include<stdio.h>
+//imports string related functions
 #include<string.h>
+//imports boolean values
 #include<stdbool.h>
 
+//this is a function which returns true or false depending if the word is a palindrome
 bool isPalindrome(int len,char word[]) {
+    //j is the index from right to left
     int j = len -1;
+    //count of the letters which are same from both directions
     int count = 0;
+    //i increments till middle of the word
     for (int i = 0; i < len/2; i++) {
+        //checks if index i of word is equal to index j of word
         if (word[i] == word[j]) {
+            //if yes, the count var will increment
             count++;
         }
+        //j decrements till its value is equal to i
         j--;
     }
+    //this code determinds if the word is a palindrome or not
     if (count >= len/2) {
         return true;
     }
@@ -20,8 +31,12 @@ bool isPalindrome(int len,char word[]) {
 }
 
 int main() {
+    //declares string variable with value "madam"
     char* word = "madam";
+    //length of the word
     int len = strlen(word);
+    //stores the returned output from the function into a boolean var called result
     bool result = isPalindrome(len, word);
+    //prints 1 for true and 0 for false
     printf("%d", result);
 }
